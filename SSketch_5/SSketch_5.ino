8bfc0fa4bf0389a42a0f90704fda73fa3f704a93
TarnishedAyame <hanakoutada3232@gmail.com>
2025-11-19 10:39:32 -0800

    changed some stuff

--- SSketch_5/SSketch_5.ino
+++ SSketch_5/SSketch_5.ino
@@ -1,13 +1,13 @@
-const int buzzerPin = 1; // Pin for the piezo buzzer
-int frequency = 0;       // Variable to store the frequency value from p5.js
+const int buzzerPin = 1; 
+int frequency = 0;  
 
 void setup() {
-  Serial.begin(9600);    // Initialize serial communication at 9600 baud rate
-  pinMode(buzzerPin, OUTPUT); // Set the buzzer pin as an output
+  Serial.begin(9600); 
+  pinMode(buzzerPin, OUTPUT); 
 }
 
 void loop() {
-  if (Serial.available() > 0) { // Check if data is available to read
+  if (Serial.available() > 0) {
     // Read the incoming byte (0-255) from the serial port
     int inByte = Serial.read(); 
 
